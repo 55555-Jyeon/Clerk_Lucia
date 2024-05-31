122d594211d3163fd93758b4cf7f9519418ebf23
55555-Jyeon <jyeon380516@gmail.com>
2024-05-31 20:15:59 +0900

    test clerk auth sample and test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
       <ClerkProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
       </ClerkProvider>
     </html>
   );
